eb6e8e9cdd7f826c38eb6616b5404947cd99b927
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-08-31 20:54:25 -0300

    Adicionado banco de dados com docjer

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE into tb_roles (id, name) values (1, 'admin');
+INSERT IGNORE into tb_roles (id, name) values (2, 'client');
+INSERT IGNORE into tb_roles (id, name) values (3, 'barber');
