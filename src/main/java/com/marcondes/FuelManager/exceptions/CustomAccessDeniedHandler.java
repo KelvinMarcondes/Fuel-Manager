7d9a269ecdec98e90ea211f5ff585bdcc50596ab
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-08-31 22:58:03 -0300

    Implementado  SpringSecurity e parcialmente a validação do login por tokenJWT

--- src/main/java/com/marcondes/FuelManager/exceptions/CustomAccessDeniedHandler.java
+++ src/main/java/com/marcondes/FuelManager/exceptions/CustomAccessDeniedHandler.java
@@ -1,30 +1,30 @@
-//package com.marcondes.FuelManager.exceptions;
-//
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.security.access.AccessDeniedException;
-//import org.springframework.security.web.access.AccessDeniedHandler;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.IOException;
-//import java.io.PrintWriter;
-//
-//@Component
-//public class CustomAccessDeniedHandler implements AccessDeniedHandler {
-//
-////    @Override
-////    public void handle(HttpServletRequest request,
-////                       HttpServletResponse response,
-////                       AccessDeniedException accessDeniedException) throws IOException {
-////
-////        response.setStatus(HttpStatus.FORBIDDEN.value());
-////        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-////
-////        PrintWriter out = response.getWriter();
-////        out.print("{\"error\": \"Acesso negado: " + accessDeniedException.getMessage() + "\"}");
-////        out.flush();
-////    }
-//
-//}
+package com.marcondes.FuelManager.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        PrintWriter out = response.getWriter();
+        out.print("{\"error\": \"Acesso negado: " + accessDeniedException.getMessage() + "\"}");
+        out.flush();
+    }
+
+}
