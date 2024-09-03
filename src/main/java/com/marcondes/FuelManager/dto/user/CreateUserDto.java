4b442c08c73a267cafd737dfbc322599a77070b0
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-09-03 12:48:30 -0300

    removida as roles CreateUserDto

--- src/main/java/com/marcondes/FuelManager/dto/user/CreateUserDto.java
+++ src/main/java/com/marcondes/FuelManager/dto/user/CreateUserDto.java
@@ -1,15 +1,10 @@
 package com.marcondes.FuelManager.dto.user;
 
-import com.marcondes.FuelManager.entities.Role;
 import lombok.Data;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 public class CreateUserDto {
    private String name;
    private String email;
    private String password;
-   private Set<Role> role = new HashSet<>();
 }
